06965c8a6adcfa0fcd90e8524ca337c386dbc7bb
Karan <bhawanijanagal@gmail.com>
2022-03-17 17:51:05 +0530

    git init

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { BrowserRouter as Router, Switch,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import ProductsPage from './pages/ProductsPage';
+import Cart from './pages/Cart';
+import SingleProduct from './pages/SingleProduct';
+import {CartContext} from './CartContext';
+import Navigation from './components/Navigation';
+import { useEffect, useState } from 'react';
+import { getCart,storeCart } from './pages/Helpers';
+
+function App(){
+
+    const [cart , setCart] = useState({});
+
+    useEffect(() =>{
+        getCart().then(cart =>{
+            setCart(JSON.parse(cart));
+
+        });
+
+    },[]);
+
+    useEffect(() =>{
+        storeCart(JSON.stringify(cart));
+    },[cart]);
+
+    return (
+        <>  
+            <Router>
+                <CartContext.Provider value={{cart,setCart}}>
+                <Navigation/>
+                 
+                 <Switch>
+                     <Route path="/" component={Home} exact></Route>
+                     {/* <Route path="/about" component={About}></Route> */}
+                     <Route path="/products" exact component={ProductsPage}></Route>
+                     <Route path="/products/:_id" component={SingleProduct}></Route>
+                     <Route path="/cart" component={Cart}></Route>
+ 
+                 </Switch>
+                </CartContext.Provider>
+                
+            </Router>
+        </>
+    )
+    
+}
+
+export default App;
